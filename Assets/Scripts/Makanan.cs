7458ed57346ecfa095d6151237be5c818423f6ee
Agung Gunawan <agg.nst772@gmail.com>
2023-04-19 23:07:21 +0700

    up

--- Assets/Scripts/Makanan.cs
+++ Assets/Scripts/Makanan.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Makanan : MonoBehaviour
 {
-    public Sprite fotoMakanan;
+    Sprite fotoMakanan;
 
     public string namaMakanan;
     public string deskripsiMakanan;
 
     public void TransferData()
     {
+        fotoMakanan = GetComponent<Image>().sprite;
         BookUI.instance.itemUISC.DetailMakananUI(fotoMakanan, namaMakanan, deskripsiMakanan);
     }
 }
--- Assets/Scripts/Minuman.cs
+++ Assets/Scripts/Minuman.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Minuman : MonoBehaviour
 {
-    public Sprite fotoMinuman;
+    Sprite fotoMinuman;
 
     public string namaMinuman;
     public string deskripsiMinuman;
 
     public void TransferData()
     {
+        fotoMinuman = GetComponent<Image>().sprite;
         BookUI.instance.itemUISC.DetailMinumanUI(fotoMinuman, namaMinuman, deskripsiMinuman);
     }
 }
