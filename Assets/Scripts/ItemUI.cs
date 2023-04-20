9f2003f046a069c8c9505b125d7547b1f7723313
Agung Gunawan <agg.nst772@gmail.com>
2023-04-20 10:54:56 +0700

    up

--- Assets/Scripts/ItemUI.cs
+++ Assets/Scripts/ItemUI.cs
@@ -15,20 +15,30 @@ public class ItemUI : MonoBehaviour
     GameObject detailMinumanUI;
 
     [SerializeField]
-    GameObject bajuUI;
+    GameObject pakaianUI;
     [SerializeField]
-    GameObject detailBajuUI;
+    GameObject detailPakaianUI;
 
     [SerializeField]
     GameObject obatUI;
     [SerializeField]
     GameObject detailObatUI;
 
+    [SerializeField]
+    RectTransform makananButton;
+    [SerializeField]
+    RectTransform minumanButton;
+    [SerializeField]
+    RectTransform pakaianButton;
+    [SerializeField]
+    RectTransform obatButton;
+
     public void MakananUI()
     {
         FalseUI();
 
         makananUI.SetActive(true);
+        FalseButton("makananButton");
     }
     public void DetailMakananUI(Sprite foto, string nama, string deskripsi)
     {
@@ -47,6 +57,7 @@ public class ItemUI : MonoBehaviour
         FalseUI();
 
         minumanUI.SetActive(true);
+        FalseButton("minumanButton");
     }
     public void DetailMinumanUI(Sprite foto, string nama, string deskripsi)
     {
@@ -60,19 +71,20 @@ public class ItemUI : MonoBehaviour
         dmUI.nama.text = nama;
         dmUI.deskripsi.text = deskripsi;
     }
-    public void BajuUI()
+    public void PakaianUI()
     {
         FalseUI();
 
-        bajuUI.SetActive(true);
+        pakaianUI.SetActive(true);
+        FalseButton("pakaianButton");
     }
-    public void DetailBajuUI(Sprite foto, string nama, string deskripsi)
+    public void DetailPakaianUI(Sprite foto, string nama, string deskripsi)
     {
         FalseUI();
 
-        detailBajuUI.SetActive(true);
+        detailPakaianUI.SetActive(true);
 
-        var dmUI = detailBajuUI.GetComponent<DetailUI>();
+        var dmUI = detailPakaianUI.GetComponent<DetailUI>();
 
         dmUI.foto.sprite = foto;
         dmUI.nama.text = nama;
@@ -83,6 +95,7 @@ public class ItemUI : MonoBehaviour
         FalseUI();
 
         obatUI.SetActive(true);
+        FalseButton("obatButton");
     }
     public void DetailObatUI(Sprite foto, string nama, string deskripsi)
     {
@@ -104,10 +117,44 @@ public class ItemUI : MonoBehaviour
         minumanUI.SetActive(false);
         detailMinumanUI.SetActive(false);
 
-        bajuUI.SetActive(false);
-        detailBajuUI.SetActive(false);
+        pakaianUI.SetActive(false);
+        detailPakaianUI.SetActive(false);
 
         obatUI.SetActive(false);
         detailObatUI.SetActive(false);
+    }
+
+    Vector2 posButton;
+    bool savePosButton;
+    void FalseButton(string namaButton)
+    {
+        if (!savePosButton)
+        {
+            savePosButton = true;
+            posButton = makananButton.sizeDelta;
+        }
+
+        makananButton.sizeDelta = posButton;
+        minumanButton.sizeDelta = posButton;
+        pakaianButton.sizeDelta = posButton;
+        obatButton.sizeDelta = posButton;
+
+        float sizeClick = 1.1f;
+        if (namaButton == "makananButton")
+        {
+            makananButton.sizeDelta = posButton * sizeClick;
+        }
+        else if (namaButton == "minumanButton")
+        {
+            minumanButton.sizeDelta = posButton * sizeClick;
+        }
+        else if (namaButton == "pakaianButton")
+        {
+            pakaianButton.sizeDelta = posButton * sizeClick;
+        }
+        else if (namaButton == "obatButton")
+        {
+            obatButton.sizeDelta = posButton * sizeClick;
+        }
     }
 }
