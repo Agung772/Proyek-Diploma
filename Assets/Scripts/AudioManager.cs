7fe50b76a7e9afbc43a8eeb3d33a9ad6209c3bed
Agung Gunawan <agg.nst772@gmail.com>
2023-04-21 11:41:55 +0700

    up

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    float volumeBGM;
+    [SerializeField]
+    float volumeSFX;
+
+    [SerializeField]
+    AudioSource audioSourceBGM;
+    [SerializeField]
+    AudioSource audioSourceSFX;
+
+    [SerializeField]
+    Slider sliderBGM;
+    [SerializeField]
+    Slider sliderSFX;
+
+    string _VolumeBGM = "VolumeBGM";
+    string _VolumeSFX = "VolumeSFX";
+    string _DefaultVolume = "DefaultVolume";
+
+    private void Start()
     {
-        
+        if (PlayerPrefs.GetFloat(_DefaultVolume) == 0)
+        {
+            PlayerPrefs.SetFloat(_DefaultVolume, 1);
+            PlayerPrefs.SetFloat(_VolumeBGM, 0.6f);
+            PlayerPrefs.SetFloat(_VolumeSFX, 0.6f);
+        }
+
+        LoadVolume();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadVolume()
     {
-        
+        volumeBGM = PlayerPrefs.GetFloat(_VolumeBGM);
+        volumeSFX = PlayerPrefs.GetFloat(_VolumeSFX);
+
+        audioSourceBGM.volume = volumeBGM;
+        audioSourceSFX.volume = volumeSFX;
+
+        sliderBGM.value = volumeBGM;
+        sliderSFX.value = volumeSFX;
+    }
+    public void ValueBGM(float value)
+    {
+        volumeBGM = value;
+        audioSourceBGM.volume = volumeBGM;
+        PlayerPrefs.SetFloat(_VolumeBGM, volumeBGM);
+        volumeBGM = Mathf.Clamp(volumeBGM, 0, 1);
+    }
+    public void ValueSFX(float value)
+    {
+        volumeSFX = value;
+        audioSourceSFX.volume = volumeSFX;
+        PlayerPrefs.SetFloat(_VolumeSFX, volumeSFX);
+        volumeSFX = Mathf.Clamp(volumeSFX, 0, 1);
     }
 }
